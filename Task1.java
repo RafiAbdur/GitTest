c55b0a896a6e2a528d99a2369004759eca4fe4dd
Shovon Rozario <shovon.rozario@monstar-lab.com>
2015-08-12 11:54:33 +0900

    Added New Human Feature

--- Task1.java
+++ Task1.java
@@ -3,6 +3,7 @@ public static void main(String []args){
 
 Human h = new Human("Tanzil");
 h.sayName();
+getNewHuman().sayName();
 }
 
 public static String feature1(int id){
@@ -11,6 +12,10 @@ return "" + id;
 
 public static String feature2(String s){
 return s;
+}
+
+public static Human getNewHuman(){
+return new Human("Alien");
 }
 
 }
